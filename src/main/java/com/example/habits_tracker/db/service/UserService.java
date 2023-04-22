d68d0cd5f1a564b9336ee2797909829a16bafd26
user <abdymomunovargen6@gmail.com>
2023-04-22 17:09:18 +0600

    fixed

--- src/main/java/com/example/habits_tracker/db/service/UserService.java
+++ src/main/java/com/example/habits_tracker/db/service/UserService.java
@@ -1,4 +1,7 @@
 package com.example.habits_tracker.db.service;
 
 public interface UserService {
+
+
+
 }
---
+++ src/main/java/com/example/habits_tracker/dto/request/SingUpRequest.java
@@ -0,0 +1,35 @@
+package com.example.habits_tracker.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SingUpRequest {
+
+    @NotNull(message = "Name must not be null!")
+    @NotBlank(message = "Name must not be empty!")
+    private String name;
+
+    @NotNull(message = "Last name must not be null!")
+    @NotBlank(message = "Last name must not be empty!")
+    private String lastname;
+
+    @NotNull(message = "Email must not be null!")
+    @NotBlank(message = "Email must not be empty!")
+    @Email(message = "@gmail.com must be!")
+    private String email;
+
+    @NotNull(message = "Password must not be null!")
+    @NotBlank(message = "Password must not be empty!")
+    private String password;
+
+}
