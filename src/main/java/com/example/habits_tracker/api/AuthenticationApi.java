942a8df7928ba695923fb9772ccd89e4e6b765ae
user <abdymomunovargen6@gmail.com>
2023-04-26 16:19:51 +0600

    forgot password and reset password

--- src/main/java/com/example/habits_tracker/api/AuthenticationApi.java
+++ src/main/java/com/example/habits_tracker/api/AuthenticationApi.java
@@ -2,14 +2,14 @@ package com.example.habits_tracker.api;
 
 import com.example.habits_tracker.db.service.UserService;
 import com.example.habits_tracker.dto.request.AuthRequest;
+import com.example.habits_tracker.dto.request.ResetPasswordRequest;
 import com.example.habits_tracker.dto.request.SignUpRequest;
 import com.example.habits_tracker.dto.response.AuthResponse;
+import com.example.habits_tracker.dto.response.SimpleResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import javax.validation.Valid;
 
 @RestController
@@ -28,4 +28,15 @@ public class AuthenticationApi {
     public AuthResponse signIn(@RequestBody @Valid AuthRequest request) {
         return userService.login(request);
     }
+
+    @PostMapping("/forgot")
+    public SimpleResponse forgotPassword(@RequestParam String email) throws MessagingException {
+        return userService.forgotPassword(email);
+    }
+
+    @PutMapping("/")
+    public SimpleResponse resetPassword(@RequestParam int verifyNumber, @RequestBody ResetPasswordRequest request) throws MessagingException {
+        return userService.resetPassword(verifyNumber, request);
+    }
+
 }
--- src/main/java/com/example/habits_tracker/db/service/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/example/habits_tracker/db/service/serviceimpl/UserServiceImpl.java
@@ -5,15 +5,23 @@ import com.example.habits_tracker.db.entities.User;
 import com.example.habits_tracker.db.repository.UserRepository;
 import com.example.habits_tracker.db.service.UserService;
 import com.example.habits_tracker.dto.request.AuthRequest;
+import com.example.habits_tracker.dto.request.ResetPasswordRequest;
 import com.example.habits_tracker.dto.request.SignUpRequest;
 import com.example.habits_tracker.dto.response.AuthResponse;
+import com.example.habits_tracker.dto.response.SimpleResponse;
 import com.example.habits_tracker.exceptions.BadCredentialsException;
 import com.example.habits_tracker.exceptions.BadRequestException;
 import com.example.habits_tracker.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.Random;
 
 @Service
 @Slf4j
@@ -25,6 +33,12 @@ public class UserServiceImpl implements UserService {
     private final JwtTokenProvider jwtTokenProvider;
 
     private final PasswordEncoder passwordEncoder;
+
+    private final CodeRepository codeRepository;
+
+    private final JavaMailSender javaMailSender;
+
+    private int randomNumber;
 
     @Override
     public AuthResponse singUp(SignUpRequest singUpRequest) {
@@ -67,6 +81,48 @@ public class UserServiceImpl implements UserService {
         } else {
             throw new BadCredentialsException("This password already exists!");
         }
+    }
+
+    @Override
+    public SimpleResponse forgotPassword(String email) throws MessagingException {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new NotFoundException(String.format("User with email: %s not found!", email))
+        );
+
+        randomNumber = new Random().nextInt(100000, 999999);
+        user.setCode(randomNumber);
+        userRepository.save(user);
+
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+        helper.setSubject("Habit Tracker");
+        helper.setTo(email);
+        helper.setText("This is a four digit verification code.: " + randomNumber);
+        javaMailSender.send(mimeMessage);
+
+        return new SimpleResponse("You have been sent a verification code!");
+    }
+
+    @Override
+    public SimpleResponse resetPassword(int verifyNumber, ResetPasswordRequest request) {
+        if (verifyNumber != randomNumber) {
+            throw new BadCredentialsException(String.format("Unfortunately this code: %d is not correct", verifyNumber));
+        }
+
+        User user = userRepository.findById(request.getId()).orElseThrow(
+                () -> new NotFoundException(String.format("User with id: %d not found!", request.getId()))
+        );
+
+        String newPassword = passwordEncoder.encode(request.getNewPassword());
+
+        if (user.getPassword().equals(newPassword)) {
+            throw new BadRequestException("The password cannot be a duplicate!");
+        } else {
+            user.setPassword(newPassword);
+            userRepository.save(user);
+            return new SimpleResponse("Password updated successfully!");
+        }
+
     }
 
 }
